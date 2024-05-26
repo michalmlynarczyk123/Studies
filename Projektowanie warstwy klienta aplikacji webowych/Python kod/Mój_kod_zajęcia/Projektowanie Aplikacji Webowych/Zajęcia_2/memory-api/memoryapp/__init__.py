3670c99d86b8646f06d31dea7633807720ad0003
michal <michalmlyn@gmail.com>
2024-05-26 10:31:11 +0200

    Update

--- Projektowanie warstwy klienta aplikacji webowych/Python kod/Mój_kod_zajęcia/Projektowanie Aplikacji Webowych/Zajęcia_2/memory-api/memoryapp/__init__.py
+++ Projektowanie warstwy klienta aplikacji webowych/Python kod/Mój_kod_zajęcia/Projektowanie Aplikacji Webowych/Zajęcia_2/memory-api/memoryapp/__init__.py
@@ -1,6 +1,34 @@
 from flask import Flask
 from flask_cors import CORS
+from sqlalchemy.orm import DeclarativeBase
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.debug = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///memory.db'
+app.config['SQLALCHEMY_ECHO'] = True
+
+db.init_app(app)
+
+
+@event.listens_for(Engine, 'connect')
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute('PRAGMA foreign_keys=ON')
+    cursor.close()
+
+
+with app.app_context():
+    db.create_all()
