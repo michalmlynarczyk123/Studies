14dcbad328ba2ed9e0b2b950da053d4072867ac8
michal <michalmlyn@gmail.com>
2024-05-26 12:18:08 +0200

    Update

--- Projektowanie warstwy klienta aplikacji webowych/Python kod/Mój_kod_zajęcia/Projektowanie Aplikacji Webowych/Zajęcia_2/memory-api/memoryapp/repository.py
+++ Projektowanie warstwy klienta aplikacji webowych/Python kod/Mój_kod_zajęcia/Projektowanie Aplikacji Webowych/Zajęcia_2/memory-api/memoryapp/repository.py
@@ -3,3 +3,11 @@ from memoryapp import db
 
 def get_categories() -> list[Category]:
     return db.session.execute(db.select(Category)).scalars().all()
+
+def create_category(category_name: str) -> Category:
+    category = Category(name=category_name)
+
+    db.session.add(category)
+    db.session.commit()
+
+    return category
--- Projektowanie warstwy klienta aplikacji webowych/Python kod/Mój_kod_zajęcia/Projektowanie Aplikacji Webowych/Zajęcia_2/memory-api/memoryapp/routes/categories.py
+++ Projektowanie warstwy klienta aplikacji webowych/Python kod/Mój_kod_zajęcia/Projektowanie Aplikacji Webowych/Zajęcia_2/memory-api/memoryapp/routes/categories.py
@@ -1,7 +1,15 @@
-from flask import jsonify
+from flask import jsonify, request
 from memoryapp import app
 from memoryapp.repository import *
 
 @app.route('/categories', methods=['GET'])
 def categories():
     return jsonify(get_categories())
+
+@app.route('/categories', methods=['POST'])
+def add_category():
+    r = request.json
+    category_name = r['name']
+
+    new_category = create_category(category_name)
+    return jsonify(new_category), 201
