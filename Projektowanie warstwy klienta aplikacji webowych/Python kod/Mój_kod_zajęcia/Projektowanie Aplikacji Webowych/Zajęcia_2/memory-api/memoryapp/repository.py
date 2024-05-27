e9388ff1aaf713e624460c00d66a83dcf4e458da
michal <michalmlyn@gmail.com>
2024-05-27 19:03:45 +0200

    Update

--- Projektowanie warstwy klienta aplikacji webowych/Python kod/Mój_kod_zajęcia/Projektowanie Aplikacji Webowych/Zajęcia_2/memory-api/memoryapp/repository.py
+++ Projektowanie warstwy klienta aplikacji webowych/Python kod/Mój_kod_zajęcia/Projektowanie Aplikacji Webowych/Zajęcia_2/memory-api/memoryapp/repository.py
@@ -1,11 +1,14 @@
 from memoryapp.entities import Category
 from memoryapp import db
 
+
 def get_categories() -> list[Category]:
     return db.session.execute(db.select(Category)).scalars().all()
 
+
 def get_category_by_id(category_id: int) -> Category:
     return db.get_or_404(Category, category_id)
+
 
 def create_category(category_name: str) -> Category:
     category = Category(name=category_name)
@@ -15,3 +18,9 @@ def create_category(category_name: str) -> Category:
 
     return category
 
+
+def delete_category(category_id: int):
+    category = db.get_or_404(Category, category_id)
+
+    db.session.delete(category)
+    db.session.commit()
--- Projektowanie warstwy klienta aplikacji webowych/Python kod/Mój_kod_zajęcia/Projektowanie Aplikacji Webowych/Zajęcia_2/memory-api/memoryapp/routes/categories.py
+++ Projektowanie warstwy klienta aplikacji webowych/Python kod/Mój_kod_zajęcia/Projektowanie Aplikacji Webowych/Zajęcia_2/memory-api/memoryapp/routes/categories.py
@@ -2,13 +2,16 @@ from flask import jsonify, request
 from memoryapp import app
 from memoryapp.repository import *
 
+
 @app.route('/categories', methods=['GET'])
 def categories():
     return jsonify(get_categories())
 
+
 @app.route('/categories/<int:category_id>', methods=['GET'])
 def get_category(category_id):
     return jsonify(get_category_by_id(category_id))
+
 
 @app.route('/categories', methods=['POST'])
 def add_category():
@@ -17,3 +20,9 @@ def add_category():
 
     new_category = create_category(category_name)
     return jsonify(new_category), 201
+
+
+@app.route('/categories/<int:category_id>', methods=['DELETE'])
+def remove_category(category_id):
+    delete_category(category_id)
+    return '', 204
