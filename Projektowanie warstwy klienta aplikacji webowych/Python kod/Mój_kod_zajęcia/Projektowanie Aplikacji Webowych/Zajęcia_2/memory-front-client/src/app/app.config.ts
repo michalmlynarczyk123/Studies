3a31ccc077ac600d7672fb872b1e5fe6086dae01
michal <michalmlyn@gmail.com>
2024-05-30 11:06:02 +0200

    update

--- Projektowanie warstwy klienta aplikacji webowych/Python kod/Mój_kod_zajęcia/Projektowanie Aplikacji Webowych/Zajęcia_2/memory-front-client/src/app/app.config.ts
+++ Projektowanie warstwy klienta aplikacji webowych/Python kod/Mój_kod_zajęcia/Projektowanie Aplikacji Webowych/Zajęcia_2/memory-front-client/src/app/app.config.ts
@@ -2,18 +2,23 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideIcons, provideNgIconsConfig} from "@ng-icons/core";
+import { provideIcons, provideNgIconsConfig } from '@ng-icons/core';
 import {
   heroArrowLeft,
   heroArrowPath,
-  heroCheckCircle, heroFaceFrown, heroFaceSmile,
+  heroCheckCircle,
+  heroFaceFrown,
+  heroFaceSmile,
   heroFolderOpen,
   heroNoSymbol,
-  heroPlusCircle, heroRocketLaunch
-} from "@ng-icons/heroicons/outline";
+  heroPlusCircle,
+  heroRocketLaunch,
+} from '@ng-icons/heroicons/outline';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideNgIconsConfig({ size: '1.5em' }),
+  providers: [
+    provideRouter(routes),
+    provideNgIconsConfig({ size: '1.5em' }),
     provideIcons({
       heroArrowLeft,
       heroArrowPath,
@@ -24,5 +29,6 @@ export const appConfig: ApplicationConfig = {
       heroRocketLaunch,
       heroFaceFrown,
       heroFaceSmile,
-    }),]
+    }),
+  ],
 };
