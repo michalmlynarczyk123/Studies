65de4c823b6c048adb0978cdbf491501c53e43b5
michal <michalmlyn@gmail.com>
2024-05-31 09:45:36 +0200

    update

--- Projektowanie warstwy klienta aplikacji webowych/Python kod/Mój_kod_zajęcia/Projektowanie Aplikacji Webowych/Zajęcia_2/memory-front-client/src/app/wordbank/categories/categories.component.ts
+++ Projektowanie warstwy klienta aplikacji webowych/Python kod/Mój_kod_zajęcia/Projektowanie Aplikacji Webowych/Zajęcia_2/memory-front-client/src/app/wordbank/categories/categories.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { NgIcon } from '@ng-icons/core';
 import { RouterLink } from '@angular/router';
 import { NgForOf, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [NgIcon, RouterLink, NgForOf, NgIf],
+  imports: [NgIcon, RouterLink, NgForOf, NgIf, FormsModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css',
 })
 export class CategoriesComponent {
-  categories = ['Kategoria 1', 'Kategoria 2'];
+  categories = ['Kategoria 1'];
+  showNewCategoryInput = false;
+  newCategoryName = '';
+
+  addCategory() {
+    this.categories.push(this.newCategoryName);
+    this.newCategoryName = '';
+    this.showNewCategoryInput = false;
+  }
 }
