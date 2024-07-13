987ed465f1476909236cfb9838b1c1fe677850f2
michal <michalmlyn@gmail.com>
2024-07-13 16:00:49 +0200

    Update

--- Zaawansowane aplikacje webowe z wykorzystaniem Django/django_wprowadzenie/basics/urls.py
+++ Zaawansowane aplikacje webowe z wykorzystaniem Django/django_wprowadzenie/basics/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('hello-world', views.hello_world),
     path('current-date', views.current_date),
     path('view-hits', views.view_hits),
-    path('country/<country_name>', views.find_country_by_name)
+    path('country/<int:country_index>', views.find_country_by_index),
+    path('country/<str:country_name>', views.find_country_by_name, name='country_by_name_url'),
+    path('first-html', views.first_html, name='first_html_url'),
+    path('first-template', views.first_template, name='first_template_url')
 ]
--- Zaawansowane aplikacje webowe z wykorzystaniem Django/django_wprowadzenie/basics/views.py
+++ Zaawansowane aplikacje webowe z wykorzystaniem Django/django_wprowadzenie/basics/views.py
@@ -1,8 +1,10 @@
 import datetime
 
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
-from .models import find_by_name
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+from django.template.loader import render_to_string
+from .models import find_by_name, all_countries
 
 
 # Create your views here.
@@ -30,3 +32,37 @@ def find_country_by_name(request, country_name):
         return HttpResponseNotFound(f'Kraj {country_name} nie został znaleziony')
 
     return HttpResponse(f'{found_country["country"]} {found_country["capital"]}')
+
+
+def find_country_by_index(request, country_index):
+    try:
+        found_country = all_countries[country_index]
+        redirect_path = reverse('country_by_name_url', args=[found_country["country"]])
+        # return HttpResponse(f'{found_country["country"]} {found_country["capital"]}')
+        print(redirect_path)
+        return HttpResponseRedirect(redirect_path)
+    except:
+        return HttpResponseNotFound(f'Kraj o indeksie {country_index} nie został znaleziony')
+
+
+def first_html(request):
+    response_data = """
+        <h1>To jest HTML</h1>
+        <ul>
+            <li>Jeden</li>
+            <li>Dwa</li>
+            <li>Trzy</li>
+        </ul>
+    """
+
+    response_data += '<h1>Dodane</h1>'
+    for i in range(10):
+        response_data += f'<p>{i}</p>'
+
+    return HttpResponse(response_data)
+
+
+def first_template(request):
+    response_data = render_to_string('basics/first_template.html')
+    print(response_data)
+    return HttpResponse(response_data)
