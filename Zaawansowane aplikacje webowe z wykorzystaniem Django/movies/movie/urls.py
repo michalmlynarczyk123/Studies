37b4cab8ff6fe6ea6d2f6fc4d23ae35c7e5ba391
michal <michalmlyn@gmail.com>
2024-07-16 08:44:10 +0200

    Update

--- Zaawansowane aplikacje webowe z wykorzystaniem Django/movies/movie/urls.py
+++ Zaawansowane aplikacje webowe z wykorzystaniem Django/movies/movie/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_movies, name='all_movies_url')
+    path('', views.all_movies, name='all_movies_url'),
+    path('<tmdb_id>', views.movie_details, name='movie_details_url')
 ]
--- Zaawansowane aplikacje webowe z wykorzystaniem Django/movies/movie/views.py
+++ Zaawansowane aplikacje webowe z wykorzystaniem Django/movies/movie/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Movie
+from django.http import HttpResponseNotFound
 
 
 # Create your views here.
@@ -10,3 +11,14 @@ def all_movies(request):
         'movies': found_movies
     }
     return render(request, 'movie/all_movies.html', context)
+
+
+def movie_details(request, tmdb_id):
+    found_movie = Movie.find_all_movies()
+    for m in found_movie:
+        if m.tmdb_id == tmdb_id:
+            context = {
+                'movie': m
+            }
+            return render(request, 'movie/movie_details.html', context)
+    return HttpResponseNotFound(f'Film nie został znaleziony')
