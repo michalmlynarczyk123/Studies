ea3674d3d43d4194f8d2296ca3d8b32ff38e7dd3
michal <michalmlyn@gmail.com>
2024-07-16 13:11:29 +0200

    Update

---
+++ Zaawansowane aplikacje webowe z wykorzystaniem Django/movies/books/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    publish_date = models.DateField()
+    vote_average = models.DecimalField(max_digits=5, decimal_places=2)
+    vote_count = models.IntegerField()
+    iban = models.CharField(max_length=10, null=True)
+    publication_country = models.CharField(max_length=255, default='Polska')
